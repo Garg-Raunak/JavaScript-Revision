c16f2e3e43a677d73cba1d0b4387a025a3b1896d
Raunakgarg.dev <gargraunak18@gmail.com>
2025-09-11 22:40:34 +0530

    Adding StrictMode.js

---
+++ StrictMode.js
@@ -0,0 +1,7 @@
+"use strict"
+document.write("<p style=\"color:red\">STRICT MODE IN JavaScript</p>")
+let xw = 5;
+
+document.write("<br />Strict mode demo "+xw)
+document.write("<br />As xz is not declared we can see the error in the console")
+//xz = 6;
