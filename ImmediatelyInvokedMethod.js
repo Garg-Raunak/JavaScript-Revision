718775763c9047c47c22c7a54c43b64783c19494
Raunakgarg.dev <gargraunak18@gmail.com>
2025-09-12 21:59:25 +0530

    Adding immediatelymethod.js

---
+++ ImmediatelyInvokedMethod.js
@@ -0,0 +1,5 @@
+(function (){
+    document.write("<h3 style='color:red'>This method is invoked immediately for the code efficiency. (V.IMP) </h3>")
+    document.write("<h3 style='color:red'>To avoid the confilcts between the names used for variables in  different-different JS files in our project this plays are very important role !!! </h3>")
+    document.write("<br/>")
+})();
