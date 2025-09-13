367e46d0fd9c9156bdb62b19245110ff568279b2
Raunakgarg.dev <gargraunak18@gmail.com>
2025-09-14 00:18:26 +0530

    Adding LabelsInJS

---
+++ LablesInJS.js
@@ -0,0 +1,14 @@
+
+
+document.write("<br /> <span style=\"color:red\">LablesInJS Example :- Please check console </span>")
+//Here Raunak is lable (is nothing but a name given to a block of code it can be any)
+
+
+Raunak : for(let i=1; i<=5; i++){
+    for (let j=1; j<=5; j++){
+        if(j==5){
+            break Raunak;
+        }
+        console.log("i= "+i +" j= "+j);
+    }
+}
