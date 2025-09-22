bd8fb703c361575cf5ca810d2771e9b95663953a
Raunakgarg.dev <gargraunak18@gmail.com>
2025-09-22 22:01:04 +0530

    added a fix

--- LablesInJS.js
+++ LablesInJS.js
@@ -8,7 +8,7 @@ Raunak : for(let i=1; i<=5; i++){
     for (let j=1; j<=5; j++){
         if(j==5){
             break Raunak;
-        }
+        } 
         console.log("i= "+i +" j= "+j);
     }
 }
